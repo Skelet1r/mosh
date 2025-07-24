364ff6ee998dfe180124550bf094601e49d428a2
Skelet1r <oleksiidmytrevych@gmail.com>
2025-07-24 17:54:55 +0300

    Added 'cd' command and also if 'cd' will be without arguments you will go to the home directory

--- exec_commands.c
+++ exec_commands.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 #include "includes/word_item.h"
+#include "includes/helpers.h"
 
 static int count_list_elements(struct word_item* head)
 {
@@ -37,10 +39,58 @@ static char** list_to_array(struct word_item** head)
     return cmdline;
 }
 
+static int check_chdir(const char* dir_path, struct word_item** curr)
+{
+    int chdir_result;
+    chdir_result = chdir(dir_path);
+    if (chdir_result == -1) {
+        perror("chdir");                
+        return ERROR_CODE;
+    }
+    return SUCCESS_CODE;
+}
+
+static int exec_cd(struct word_item** head)
+{
+    struct word_item* curr;
+    int i;
+
+    i = 0;
+    curr = *head;
+    
+    if (!strcmp(curr->word, "cd")) {
+        while (curr != NULL) {
+            if (i == 0) {
+                if (curr->next == NULL) {
+                    const char* home_path = getenv("HOME");
+                    check_chdir(home_path, &curr);
+                }
+            }
+
+            if (i == 1) {
+                const char* dir_path = curr->word;
+                check_chdir(dir_path, &curr);
+            }
+
+            if (i == 2) {
+                return SUCCESS_CODE;
+            }
+            i++;
+            curr = curr->next;
+        }
+    }
+    return ERROR_CODE;
+}
+
 void exec_commands(struct word_item** head)
 {
     char** cmdline; 
-    int pid;
+    int pid, cd;
+    
+    cd = exec_cd(head);
+    if (cd == -1) {
+        return;
+    }
 
     cmdline = list_to_array(head);
     pid = fork();
--- includes/helpers.h
+++ includes/helpers.h
@@ -1,4 +1,5 @@
 #define BUF_SIZE 1024
 #define ERROR_CODE 1
+#define SUCCESS_CODE 0
 
 void allocation_error(char* command);
